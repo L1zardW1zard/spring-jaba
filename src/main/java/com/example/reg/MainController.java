027fab9786aec58268c100f7ded03b3ac87465ee
Valentyn Shovtenko <angerdars@gmail.com>
2022-06-06 19:08:25 +0300

    first commit

---
+++ src/main/java/com/example/reg/MainController.java
@@ -0,0 +1,17 @@
+package com.example.reg;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+// Не работает
+@Controller
+public class MainController {
+
+    @GetMapping("/greeting")
+    public String greeting(@RequestParam(name="name", required=false, defaultValue="World") String name, Model model) {
+        model.addAttribute("name", name);
+        return "index"; // должно грузить страницу из templates
+    }
+}
---
+++ src/main/java/com/example/reg/UserController.java
@@ -0,0 +1,51 @@
+package com.example.reg;
+
+import com.example.reg.entity.UserEntity;
+import com.example.reg.exception.UserAlreadyExistException;
+import com.example.reg.exception.UserNotFoundException;
+import com.example.reg.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/users")
+
+public class UserController {
+
+    @Autowired
+    private UserService userService;
+
+    @GetMapping
+    public ResponseEntity getOneUser(@RequestParam Long id) {
+        try {
+            return ResponseEntity.ok(userService.getOne(id));
+        } catch (UserNotFoundException e) {
+            return ResponseEntity.badRequest().body(e.getMessage());
+        } catch (Exception e) {
+            return ResponseEntity.badRequest().body("Error");
+        }
+    }
+
+    @PostMapping
+    public ResponseEntity registration(@RequestBody UserEntity user){
+        try{
+            userService.registration(user);
+            return ResponseEntity.ok("User created");
+        }
+        catch (UserAlreadyExistException e) {
+            return ResponseEntity.badRequest().body(e.getMessage());
+        } catch (Exception e) {
+            return ResponseEntity.badRequest().body("Error");
+        }
+    }
+
+    @DeleteMapping("/delete/{id}")
+    public ResponseEntity deleteUser(@PathVariable Long id) {
+        try {
+            return ResponseEntity.ok(userService.delete(id));
+        } catch (Exception e) {
+            return ResponseEntity.badRequest().body("Произошла ошибка");
+        }
+    }
+}
